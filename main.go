7dc7e02a9c72b4778627008ba75604e24f52d6f7
Abanoub Hanna <abanoubhannaa73@gmail.com>
2019-04-15 06:24:25 +0200

    1st functional process

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"io"
+	"net/http"
 	"os"
 
 	"github.com/spf13/cobra"
@@ -12,7 +14,7 @@ func main() {
 		Short:        "Smart Internet Download Manager!",
 		SilenceUsage: true,
 	}
-	cmd.AddCommand(downloadUrlCmd())
+	cmd.AddCommand(downloadUrlCmd(), getFile())
 	if err := cmd.Execute(); err != nil {
 		os.Exit(1)
 	}
@@ -28,4 +30,43 @@ func downloadUrlCmd() *cobra.Command {
 			return nil
 		},
 	}
+}
+
+func getFile() *cobra.Command {
+	return &cobra.Command{
+		Use: "getfile",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			cmd.Println("Image Downloading..")
+			//edit to make it dynamic
+			fileUrl := "https://4.bp.blogspot.com/-TOpMgE8dQKc/W-s52dGFFXI/AAAAAAAAyaU/Z1OHgo2ZSa88E3IaF9ztZ5UsnISSi82IgCK4BGAYYCw/s1600/logo.png"
+			if err := DownloadFile("logo.jpg", fileUrl); err != nil {
+				panic(err)
+			}
+			cmd.Println("Image Downloaded.")
+			return nil
+		},
+	}
+}
+
+// DownloadFile will download a url to a local file. It's efficient because it will
+// write as it downloads and not load the whole file into memory.
+func DownloadFile(filepath string, url string) error {
+
+	// Get the data
+	resp, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	// Create the file
+	out, err := os.Create(filepath)
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+
+	// Write the body to file
+	_, err = io.Copy(out, resp.Body)
+	return err
 }
